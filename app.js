07810483ca4eed9d0e200df46d12d8c523518afa
Antonio Castellanos <ing.castean@gmail.com>
2021-12-27 13:37:12 -0700

    update 3

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ const fs = require('fs');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-app.use(cors())
+
+var allowedOrigins = ['http://localhost:3000',
+                      'https://reverent-leakey-90670b.netlify.app'];
+app.use(cors({
+    origin: function(origin, callback){
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+          var msg = 'Las politicas de CORS policy para este sitio no le permiten ' +
+                    'tener acceso al origen especificado.';
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      }
+}))
 
 app.get('/video', (req, res) => {
     res.sendFile('assets/video1.mp4', { root: __dirname });
